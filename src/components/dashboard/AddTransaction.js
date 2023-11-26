5b8ead38a0f95251fe61cab6251be45211ba10b3
Munish <hmunish5@gmail.com>
2023-11-26 16:42:52 +0200

    Add add-transaction markup & styles

--- src/components/dashboard/AddTransaction.js
+++ src/components/dashboard/AddTransaction.js
@@ -1,10 +1,20 @@
 import TopBar from '../common/TopBar';
 
 const AddTransaction = () => (
-  <main>
-    <TopBar />
-    <p>Add transaction</p>
-  </main>
+  <section className="wh-100 add-transaction">
+    <TopBar text="Add Transaction" />
+    <form className="add-transaction">
+      <input className="add-transaction" type="text" name="amount" placeholder="Amount" />
+      <input className="add-transaction" type="text" name="category" placeholder="Category" />
+      <input className="add-transaction" type="text" name="description" placeholder="Description" />
+      <div className="trc-type">
+        <button type="button" className="trc-type income">Income</button>
+        <button type="button" className="trc-type expense">Expense</button>
+      </div>
+      <input className="add-transaction" type="date" name="date" placeholder="Pick your date" />
+      <input className="add-transaction" id="submit-transaction" type="submit" value="Add Transaction" />
+    </form>
+  </section>
 );
 
 export default AddTransaction;
